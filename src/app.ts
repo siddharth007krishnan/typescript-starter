e8631079cadadc9ae1186d45d95c146ecbab6ca8
Siddharth Krishnan <siddharth007krishnan@rediffmail.com>
2020-07-27 20:53:17 +0530

    added changes

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope! ");
+  res.send("The sedulous hyena ate the antelope!");
 });
 
 app.listen(port, (err) => {
